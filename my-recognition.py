4a0080a54133f14b73aa26cd3478b245d6c3538f
Student <s50221320@hiroshima-u.ed.jp>
2025-07-30 10:31:50 -0700

    specify model

--- my-recognition.py
+++ my-recognition.py
@@ -6,10 +6,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", type=str, help="filename of the image to process")
-parser.add_argument("--network", type=str, default="resnet-18", help="model to use")
+# parser.add_argument("--network", type=str, default="resnet-18", help="model to use")
 opt = parser.parse_args()
 img = jetson_utils.loadImage(opt.filename)
-net = jetson_inference.imageNet(opt.network, model="resnet18.onnx")
+net = jetson_inference.imageNet(model="resnet18.onnx")
 class_idx, confidence = net.Classify(img)
 class_desc = net.GetClassDesc(class_idx)
 print("image is recognized as "+ str(class_desc) +" (class #"+ str(class_idx) +") with " + str(confidence*100)+"% confidence")
